7e095b9f08f76893e48a9739cb97b531f415e63c
JULIOMENA986 <20170157@uttcampus.edu.mx>
2024-06-17 16:48:55 -0600

    second commit login routes

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3312',
+        changeOrigin: true,
+        timeout: 60000,
+        secure: false,
+      }
+    }
+  }
+})
